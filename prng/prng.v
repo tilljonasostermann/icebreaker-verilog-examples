eebb16a07558c6172de627b1fc655f20af73b705
Till-Jonas Ostermann <till.ostermann@posteo.de>
2021-08-15 22:17:51 +0200

    Changed to lower frequency output.

--- prng/prng.v
+++ prng/prng.v
@@ -23,11 +23,29 @@ module top (
 	input CLK,
 	input BTN_N,
     output P1A7,
+    output P1A8,
     output LEDG_N
 );
 
+
+// Generate slow clock from 12MHz main oscillator frequency
+// Clock output test
+// clock devider
+// hardcoded comperator number can be used to create required output frequency
+// by default, the main clock is 12MHz on the iCEBreaker, the output is devided here to 10kHz
+reg [9:0] CLK_counter = 0;
+always @(posedge CLK) begin
+    CLK_counter = CLK_counter+1;
+
+    if (CLK_counter >= 600) begin
+        CLK_counter <= 0;
+        P1A8 = ~P1A8;
+    end
+end
+
+
 // Psydo random numbers
-// Small block to create a random output stream
+// Small block to create a random output stream, called on the CLK signal generated and outputed on P1A8
 reg [3:0] b;
 
 //LFSR feedback bit
@@ -35,7 +53,7 @@ wire feedback;
 assign feedback = b[0] ^ b[3];
 
 // Add active low reset to sensitivity list
-always@(posedge CLK or negedge BTN_N) begin
+always@(posedge P1A8 or negedge BTN_N) begin
  if (BTN_N==1'b0) begin
   b[3:0]<=4'hF;  //reset condition first
   //LEDG_N <= ~LEDG_N; 
@@ -46,7 +64,8 @@ always@(posedge CLK or negedge BTN_N) begin
  end
 end
 
-assign P1A7 = b[0];
-
+// assign the output signals to the ports
+assign P1A7 = b[0]; // pseudo random output
+assign P1A8 = CLK_375kHz; // 1.5MHz clock
 
 endmodule
